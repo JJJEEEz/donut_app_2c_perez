aca075b3e9f0cc854d62cafd4aab2d53bfd13f25
JJJEEEz <juanestebanperezvelazquez@gmail.com>
2025-03-07 13:16:11 -0600

    DonutTile Finished

--- lib/tabs/donut_tab.dart
+++ lib/tabs/donut_tab.dart
@@ -4,11 +4,23 @@ import 'package:flutter/material.dart';
 class DonutTab extends StatelessWidget {
   //Lista de donas
   final List donutsOnSale = [
-    // [ donutFlavor, donutPrice, donutColor, imageName ]
-    ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
-    ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
-    ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
-    ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+    // [ donutFlavor, donutStore donutPrice, donutColor, imageName ]
+    [
+      "Ice Cream",
+      "Kryspy Kreme",
+      "36",
+      Colors.blue,
+      "lib/images/icecream_donut.png"
+    ],
+    [
+      "Strawberry",
+      "Dunkin donuts",
+      "45",
+      Colors.red,
+      "lib/images/strawberry_donut.png"
+    ],
+    ["Grape Ape", "Aurrera", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "Costco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
   ];
   DonutTab({super.key});
 
@@ -25,15 +37,16 @@ class DonutTab extends StatelessWidget {
             SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           //Relacion de aspecto (proporcion)
-          childAspectRatio: 1 / 1.5,
+          childAspectRatio: 1 / 1.6,
         ),
         itemBuilder: (context, index) {
           //cada elemento individual
           return DonutTile(
             donutFlavor: donutsOnSale[index][0],
-            donutPrice: donutsOnSale[index][1],
-            donutColor: donutsOnSale[index][2],
-            imageName: donutsOnSale[index][3],
+            donutStore: donutsOnSale[index][1],
+            donutPrice: donutsOnSale[index][2],
+            donutColor: donutsOnSale[index][3],
+            imageName: donutsOnSale[index][4],
           );
         });
   }
--- lib/utils/donut_tile.dart
+++ lib/utils/donut_tile.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class DonutTile extends StatelessWidget {
   final String donutFlavor;
   final String donutPrice;
+  final String donutStore;
   // Es dynamic porque será de tipo COLOR
   final dynamic donutColor;
   final String imageName;
@@ -12,7 +13,8 @@ class DonutTile extends StatelessWidget {
       required this.donutColor,
       required this.donutFlavor,
       required this.donutPrice,
-      required this.imageName});
+      required this.imageName,
+      required this.donutStore});
 
   @override
   Widget build(BuildContext context) {
@@ -54,8 +56,44 @@ class DonutTile extends StatelessWidget {
             ),
 
             //Donut Flavor Text
+            Text(
+              donutFlavor,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            //Espacio entre textos
+            const SizedBox(
+              height: 4,
+            ),
 
+            //Donut Store Text
+            Text(donutStore, style: TextStyle(color: Colors.grey[800])),
+
+            const SizedBox(
+              height: 4,
+            ),
             //Buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Icon(
+                    Icons.favorite_border,
+                    size: 30,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Icon(
+                    Icons.add,
+                    size: 30,
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
